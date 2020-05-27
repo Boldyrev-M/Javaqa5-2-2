a44219952cd9733700db3090827fdf3e601af510
Maxim Boldyrev <maxikb@mail.ru>
2020-05-27 23:38:27 +0300

    Printout data added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         double regularBonus = 0.3;
+        System.out.println("regularBonus" + regularBonus);
         double specialBonus = 0.6;
+        System.out.println("specialBonus" + specialBonus);
         double totalBonus = regularBonus + specialBonus;
         System.out.println(totalBonus);
     }
